9e8bd2abcd75e8d5c8b95cacd70fcb7b7619d491
Evan Altshule <evanaltshule@Evans-MacBook-Pro.local>
2021-03-05 11:49:25 -0800

    Basic leader election done

--- server.py
+++ server.py
@@ -18,17 +18,28 @@ key/value data structure: normal python dictionary
 #QUEUE = collections.deque()
 #STUDENTS = dict()
 
+LOCK = threading.Lock()
+
 #Paxos globals
+LEADER = None
+CURR_BAL_NUM = (0,0,0)
+#For ballot num
 SEQ_NUM = 0
-PROC_ID = 0
+MY_PID = 0
 DEPTH = 0
+#For acceptor phase
 NUM_PROMISES = 0
-#make function that returns depth (length) of blockchain
-#leader = server_id
-#accept_count = 0
+ENOUGH_PROMISES = False
+#For promise phase
+ACCEPTED_NUM = 0
+ACCEPTED_VAL = None
+ACCEPTED_PID = 0
+
+
+ACCEPT_COUNT = 0
 
 #Blockchain variables
-#BLOCKCHAIN = []
+BLOCKCHAIN = []
 
 
 def do_exit(server_socket):
@@ -38,7 +49,7 @@ def do_exit(server_socket):
 
 
 def handle_input(listen_for_servers, server_pids, client_pids):
-    global OUR_PID
+    global MY_PID
     global OTHER_SERVERS
     while True:
         try:
@@ -48,18 +59,13 @@ def handle_input(listen_for_servers, server_pids, client_pids):
             #connect to all servers
             if inp == 'connect':
                 for pid, port in server_pids.items():
-                    if int(OUR_PID) != int(pid):
+                    if MY_PID != int(pid):
                         (threading.Thread(target=server_connect, args=(int(pid),port))).start()
                 # for pid, port in client_pids.items():
                 #     (threading.Thread(target=client_connect, args=(int(pid),port))).start()
             #Broadcast message to all servers
             if inp == 'leader':
                 threading.Thread(target = leader_request, args = (server_pids,)).start()
-            if inp[0:4] == 'send':
-                sentence = inp[6:len(inp) - 1]
-                for s_pid, conn in OTHER_SERVERS.items():
-                    print(f"Sending message from server {OUR_PID} to server {s_pid}")
-                    conn.sendall(bytes(OUR_PID + ": " + sentence, 'utf-8'))
         except EOFError:
             pass
 
@@ -74,23 +80,39 @@ def server_connect(server_pid,port):
     print(f"Connected to Server{server_pid}")
 
 def leader_request(server_pids):
-    global OUR_PID
+    global MY_PID
     global OTHER_SERVERS
+    global NUM_PROMISES
+    global CURR_BAL_NUM
+
+    increment_seq_num()
     ballot = get_ballot_num()
+    CURR_BAL_NUM = ballot
     message = {}
     message["type"] = "prepare"
-    message["ballot"] = get_ballot_num()
-    message["sender_pid"] = OUR_PID
+    message["ballot"] = ballot
+    message["sender_pid"] = MY_PID
     encoded_message = pickle.dumps(message)
+    time.sleep(4)
     for s_pid, conn in OTHER_SERVERS.items():
-        print(f"Sending prepare from server {OUR_PID} to server {s_pid}")
+        print(f"Sending prepare from server {MY_PID} to server {s_pid}")
         conn.sendall(encoded_message)
+    while(ENOUGH_PROMISES != True):
+        pass
+    leader_broadcast()
+
+
+def increment_seq_num():
+    global SEQ_NUM
+    LOCK.acquire()
+    SEQ_NUM += 1
+    LOCK.release()
 
 def get_ballot_num():
     global SEQ_NUM
-    global PROC_ID
+    global MY_PID
     global DEPTH
-    return (SEQ_NUM, PROC_ID, DEPTH)
+    return (SEQ_NUM, MY_PID, DEPTH)
 
 # def client_connect(client_pid,port):
 #     global CLIENTS
@@ -112,28 +134,76 @@ def get_ballot_num():
 #     client_socket.close()
 
 def handle_server(stream):
+    global NUM_PROMISES
+    global LEADER
+    global MY_PID
+    global ENOUGH_PROMISES
+
     while(True):
         data = stream.recv(1024)
         if not data:
             break
         message = pickle.loads(data)
         #Handle prepare message
+        increment_seq_num()
         if message["type"] == "prepare":
-            print(f"Received prepare message from server {message["sender_pid"]}")
-            threading.Thread(target=handle_prepare, args = (message,))
+            sender = message["sender_pid"]
+            print(f"Received prepare message from server {sender}")
+            threading.Thread(target=handle_prepare, args = (message,)).start()
         #Handle promise message
         elif message["type"] == "promise":
+            LOCK.acquire()
+            NUM_PROMISES += 1
+            if NUM_PROMISES >=2:
+                ENOUGH_PROMISES = True
+            LOCK.release()
+        elif message["type"] == "leader_broadcast":
+            LEADER = message["leader"]
+            print(f"Assigned leader to {LEADER}")
 
 
 
 def handle_prepare(message):
     global OTHER_SERVERS
-    conn = OTHER_SERVERS[message["sender_pid"]]
-    reply = {}
-    reply["type"] = "promise"
-    reply_encoded = pickle.dumps(reply)
-    conn.sendall(reply)
+    global DEPTH
+    global ACCEPTED_NUM
+    global ACCEPTED_PID
 
+    sender_pid = int(message["sender_pid"])
+    conn = OTHER_SERVERS[sender_pid]
+    reply = {}
+
+    proposer_ballot = message["ballot"]
+    print(f"proposer ballot: {proposer_ballot}")
+    #compare depth, seq_num, andpid of proposer to currently held values
+    if(proposer_ballot[2] < DEPTH or proposer_ballot[0] < ACCEPTED_NUM  or (proposer_ballot[0] == ACCEPTED_NUM and proposer_ballot[1] < ACCEPTED_PID)):
+        print(f"Proposal no good")
+        reply["type"] = "nack"
+        reply["ballot"] = proposer_ballot
+        reply["error"] = "prepare rejected"
+        reply_encoded = pickle.dumps(reply)
+        conn.sendall(reply_encoded)
+    #All clear to send promise
+    else:
+        CURR_BAL_NUM = proposer_ballot         
+        reply["type"] = "promise"
+        reply["promise_values"] = {"bal": CURR_BAL_NUM, "accepted_num": ACCEPTED_NUM, "accepted_val": ACCEPTED_VAL, "depth": DEPTH}
+        reply_encoded = pickle.dumps(reply)
+        print(f"Sending promise back to leader")
+        conn.sendall(reply_encoded)
+
+def leader_broadcast():
+    global MY_PID 
+
+    message = {}
+    message["type"] = "leader_broadcast"
+    message["leader"] = MY_PID
+    encoded_message = pickle.dumps(message)
+    increment_seq_num()
+    print(f"I am the leader!")
+    time.sleep(4)
+    for s_pid, conn in OTHER_SERVERS.items():
+        conn.sendall(encoded_message)
 
 def handle_client(stream):
     while(True):
@@ -152,7 +222,7 @@ if __name__ == "__main__":
         sys.exit()
 
     #Set up listening socket for current server
-    OUR_PID = sys.argv[1]
+    MY_PID = int(sys.argv[1])
     PORT = sys.argv[2]
     listen_for_servers = socket.socket()
     listen_for_servers.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
